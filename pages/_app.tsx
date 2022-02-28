c12b1fca2be4a13d5f277d2c9dcba9b18d467d5e
Diego Castro <diego.castro@evolvingweb.ca>
2022-02-27 21:08:30 -0500

    Adding Google Analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,24 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
+      <Script
+        id="ga-script-loader"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-2VP8FYRYN8`}
+      />
+      <Script
+        id="ga-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-2VP8FYRYN8');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </React.Fragment>
   )
